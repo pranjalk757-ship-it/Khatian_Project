1f2bd3c69ef4f985a0c1a0dbd98f2c6e7dbfcd32
Pranjal Kashyap <pranjalkashyap@Pranjals-MacBook-Air.local>
2026-06-09 16:40:42 +0530

    Remove leftover merge conflict marker

--- src/components/khatian-search/page.jsx
+++ src/components/khatian-search/page.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import tripuraData from '../../data'
@@ -42,212 +41,11 @@ function Khatian_search() {
         console.log("Ye raha data",data)
     }
 
->>>>>>> 787255b (Updated khatian search and services page)
   return (
-    <div className="flex flex-col gap-y-5">
-      <div>
-        <CitizenDetails />
-      </div>
-      <form
-        className="flex flex-col gap-5"
-        onSubmit={handleSubmit(submitHandler)}
-      >
-        <div className="border border-blue-100 rounded-t">
-          <div className="text-xl font-bold bg-steal-blue px-5 py-2 text-white">
-            Location Details
-          </div>
-          <div className="grid grid-cols-3 gap-y-2 p-4">
-            <div className="grid grid-cols-2 gap-5">
-              <label
-                htmlFor="district"
-                className="justify-self-end font-semibold p-2"
-              >
-                District
-              </label>
-              <select
-                name="district"
-                id="district"
-                className="border-b focus:outline-none rounded border-[1px]"
-                {...register("district", { required: true })}
-              >
-                <option value="">--select--</option>
-                {Object.keys(tripuraData).map((district) => (
-                  <option key={district} value={district}>
-                    {district}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div className="grid grid-cols-2 gap-5">
-              <label
-                htmlFor="district"
-                className="justify-self-end font-semibold p-2"
-              >
-                Sub Division
-              </label>
-              <select
-                name="subdivision"
-                className="border-b focus:outline-none rounded border-[1px]"
-                id="subdivision"
-                {...register("subdivision", { required: true })}
-              >
-                <option value="">--select--</option>
-                {selectedDistrict &&
-                  Object.keys(tripuraData?.[selectedDistrict] || [])?.map(
-                    (subdivision) => (
-                      <option key={subdivision} value={subdivision}>
-                        {subdivision}
-                      </option>
-                    ),
-                  )}
-              </select>
-            </div>
-            <div className="grid grid-cols-2 gap-5">
-              <label
-                htmlFor="district"
-                className="justify-self-end font-semibold p-2"
-              >
-                Revenue Circle
-              </label>
-              <select
-                name="revenueCircle"
-                className="border-b focus:outline-none rounded border-[1px]"
-                id="revenueCircle"
-                {...register("revenueCircle", { required: true })}
-              >
-                <option value="">--select--</option>
-                {selectedSubdivision &&
-                  Object.keys(
-                    tripuraData?.[selectedDistrict]?.[selectedSubdivision] ||
-                      [],
-                  )?.map((revenueCircle) => (
-                    <option key={revenueCircle} value={revenueCircle}>
-                      {revenueCircle}
-                    </option>
-                  ))}
-              </select>
-            </div>
-            <div className="grid grid-cols-2 gap-5">
-              <label
-                htmlFor="district"
-                className="justify-self-end font-semibold p-2"
-              >
-                Revenue Tehsil
-              </label>
-              <select
-                name="tehsil"
-                className="border-b focus:outline-none rounded border-[1px]"
-                id="tehsil"
-                {...register("tehsil", { required: true })}
-              >
-                <option value="">--select--</option>
-                {selectedRevenueCircle &&
-                  Object.keys(
-                    tripuraData?.[selectedDistrict]?.[selectedSubdivision]?.[
-                      selectedRevenueCircle
-                    ] || [],
-                  )?.map((tehsil) => (
-                    <option key={tehsil} value={tehsil}>
-                      {tehsil}
-                    </option>
-                  ))}
-              </select>
-            </div>
-            <div className="grid grid-cols-2 gap-5">
-              <label
-                htmlFor="district"
-                className="justify-self-end font-semibold p-2"
-              >
-                Revenue Mouja
-              </label>
-              <select
-                name="mouja"
-                id="mouja"
-                className="border-b focus:outline-none rounded border-[1px]"
-                {...register("mouja", { required: true })}
-              >
-                <option value="">--select--</option>
-                {selectedTehsil &&
-                  (
-                    tripuraData?.[selectedDistrict]?.[selectedSubdivision]?.[
-                      selectedRevenueCircle
-                    ]?.[selectedTehsil] || []
-                  )?.map((mouja) => (
-                    <option key={mouja} value={mouja}>
-                      {mouja}
-                    </option>
-                  ))}
-              </select>
-            </div>
-            <div className="justify-self-center">
-                <button className="bg-steal-blue px-5 py-2 rounded flex items-center active:scale-95 justify-center text-white font-bold">Go</button>
-            </div>
-          </div>
+    <div className=''>
+        <div>
+            <CitizenDetails />
         </div>
-<<<<<<< HEAD
-        <div className="border border-blue-100 rounded-t">
-          <div className="text-xl font-bold bg-steal-blue px-5 py-2 text-white">
-            ROR / Khatian   Verification
-          </div>
-          <div className="flex p-2">
-            <div className="flex items-center p-2 w-2/5 flex-col">
-              <label
-                htmlFor="inputType"
-                className="bg-pure-greys-25 font-semibold w-full flex justify-center"
-              >
-                Search By
-              </label>
-
-              <div className="w-full">
-                <select
-                  id="inputType"
-                  className="w-full border p-2 border-blue-25 focus:outline-none"
-                  {...register("inputType", { required: true })}
-                >
-                  <option value="" className="w-full">
-                    Search By
-                  </option>
-
-                  {searchby.map((element) => (
-                    <option
-                      className="bg-white"
-                      key={element.id}
-                      value={element.name}
-                    >
-                      {element.name}
-                    </option>
-                  ))}
-                </select>
-              </div>
-            </div>
-
-            <div className="flex flex-col p-2 items-center w-2/5 justify-center">
-              <label
-                htmlFor="inputText"
-                className="bg-pure-greys-25 font-semibold w-full flex justify-center"
-              >
-                Enter Detail{" "}
-              </label>
-              <input
-                type="text"
-                id="inputText"
-                placeholder="Enter Details"
-                {...register("inputText", { required: true })}
-                className="p-2 border border-blue-25 mx-2 w-full focus:outline-none"
-              />
-            </div>
-            <div className="w-1/5 flex">
-              <div className="w-1/2 justify-center items-center flex">
-                <button className="bg-steal-blue px-4 py-1 text-white cursor-pointer active:scale-95 rounded">
-                  Verify
-                </button>
-              </div>
-              <div className="w-1/2 justify-center items-center flex">
-                <button className="bg-ufo-green px-4 py-1 text-white cursor-pointer active:scale-95 rounded">
-                  Save
-                </button>
-              </div>
-=======
         <form onSubmit={handleSubmit(submitHandler)}>
             <div className='mt-10 bg-steal-blue px-6 rounded-t-[4px] py-3 text-white font-bold text-xl'>Requesting   ROR / Khatian   Location</div>
             <div className='grid grid-cols-3 bg-white border border-steal-blue  py-10 px-10 gap-y-10'>
@@ -443,19 +241,10 @@ function Khatian_search() {
             }
             <div className='bg-caribbeangreen-200 text-white font-bold text-md w-fit py-1 px-4 ml-10 mt-10 rounded-md'>
                 <button type='submit'>Submit</button>
->>>>>>> 787255b (Updated khatian search and services page)
             </div>
-          </div>
-        </div>
-        {/* <div className="bg-caribbeangreen-200 text-white font-bold text-md w-fit py-1 px-4 ml-10 mt-10 rounded-md">
-          <button type="submit">Submit</button>
-        </div> */}
-      </form>
-      <div>
-        <RrevenueDetails/>
-      </div>
+        </form>
     </div>
-  );
+  )
 }
 
-export default Khatian_search;
+export default Khatian_search
